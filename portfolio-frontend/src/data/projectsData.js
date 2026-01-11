f333aa5c178900f120f35d936d1a38455588bb8b
hazeemmohamed <mhmmdhazeem9@gmail.com>
2026-01-11 19:53:05 +0300

    Added New Project

--- portfolio-frontend/src/data/projectsData.js
+++ portfolio-frontend/src/data/projectsData.js
@@ -1,70 +1,81 @@
 const projects = [
   {
     id: 1,
-    title: "Apple Website Clone",
+    title: "MobileWorld – E-commerce App",
     description:
-      "Pixel-perfect Apple website clone focusing on layout, animations, and responsive UI.",
-    tech: ["React", "Tailwind CSS"],
-    github: "https://github.com/hazeemmohamed/apple-website-clone.git",
-    live: "https://apple-website-clone-alpha-virid.vercel.app/",
-    category: "Clone"
+      "A complete e-commerce web application for mobile products with product listing, cart, and checkout flow.",
+    tech: ["React", "Tailwind CSS", "Node.js", "MongoDB"],
+    github: "https://github.com/hazeemmohamed/ecommerce-frontend.git",
+    live: "https://ecommerce-frontend-sooty-ten.vercel.app/",
+    category: "Featured"
   },
   {
     id: 2,
-    title: "Netflix Clone",
-    description:
-      "Netflix UI clone with dynamic movie sections and modern layout.",
-    tech: ["React", "API", "CSS"],
-    github: "https://github.com/hazeemmohamed/movieflix.git",
-    live: "https://hazeemmohamed.github.io/movieflix/",
-    category: "Clone"
-  },
-  {
-    id: 3,
-    title: "Udemy Clone",
-    description:
-      "Udemy-style learning platform UI with course listings and responsive design.",
-    tech: ["React", "Tailwind"],
-    github: "https://github.com/hazeemmohamed/udemy-clone.git",
-    live: "https://hazeemmohamed.github.io/movieflix/",
-    category: "Clone"
-  },
-  {
-    id: 4,
-    title: "Task Manager App",
-    description:
-      "Task management application with add, edit, delete, and status tracking.",
-    tech: ["React", "LocalStorage"],
-    github: "https://github.com/yourname/task-manager",
-    live: "https://task-manager-app-chi-flame.vercel.app/",
-    category: "Utility"
-  },
-  {
-    id: 5,
-    title: "Weather App",
-    description:
-      "Weather forecasting app using real-time API data.",
-    tech: ["JavaScript", "API"],
-    github: "https://github.com/hazeemmohamed/weather-app.git",
-    live: "https://weather-app-omega-sandy.vercel.app/",
-    category: "Utility"
-  },
-  {
-    id: 6,
     title: "Bulk Email Sender",
     description:
       "The Bulk Email Sender application allows users to send emails to multiple recipients at once by uploading an Excel (XLSX) file containing email addresses.",
-    tech: ["React"],
+    tech: ["React", "Tailwind", "Node.js","Nodemailer","MongoDB"],
     github: "https://github.com/hazeemmohamed/bulkmail.git",
     live: "https://bulkemail-frontend.vercel.app/",
     category: "Tool"
   },
   {
+    id: 3,
+    title: "Netflix Clone",
+    description:
+      "Netflix UI clone with movie sections and modern layout.",
+    tech: ["HTML", "Tailwind CSS", "React"],
+    github: "https://github.com/hazeemmohamed/movieflix.git",
+    live: "https://hazeemmohamed.github.io/movieflix/",
+    category: "Clone"
+  },
+  {
+    id: 4,
+    title: "Udemy Clone",
+    description:
+      "Udemy-style learning platform UI with course listings and responsive design.",
+    tech: ["HTML", "CSS"],
+    github: "https://github.com/hazeemmohamed/udemy-clone.git",
+    live: "https://hazeemmohamed.github.io/movieflix/",
+    category: "Clone"
+  },
+  {
+    id: 5,
+    title: "Task Manager App",
+    description:
+      "Task management application with add, edit, delete, and status tracking.",
+    tech: ["React","Tailwind","React", "LocalStorage"],
+    github: "https://github.com/yourname/task-manager",
+    live: "https://task-manager-app-chi-flame.vercel.app/",
+    category: "Utility"
+  },
+  {
+    id: 6,
+    title: "Weather App",
+    description:
+      "Weather forecasting app using real-time API data.",
+    tech: ["React","Tailwind","JavaScript", "API"],
+    github: "https://github.com/hazeemmohamed/weather-app.git",
+    live: "https://weather-app-omega-sandy.vercel.app/",
+    category: "Utility"
+  },
+  {
     id: 7,
+    title: "Apple Website Clone",
+    description:
+      "Pixel-perfect Apple website clone focusing on layout, animations, and responsive UI.",
+    tech: ["HTML", "Tailwind CSS"],
+    github: "https://github.com/hazeemmohamed/apple-website-clone.git",
+    live: "https://apple-website-clone-alpha-virid.vercel.app/",
+    category: "Clone"
+    
+  },
+  {
+    id: 8,
     title: "Character Counter",
     description:
       "Text utility tool to count words, characters, and reading time.",
-    tech: ["JavaScript", "HTML", "CSS"],
+    tech: ["React", "Tailwind", "Javascrit"],
     github: "https://github.com/hazeemmohamed/character-counter.git",
     live: "https://hazeemmohamed.github.io/character-counter/",
     category: "Tool"
